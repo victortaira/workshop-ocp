a0a6da7b743f8dd51cd6f971ad0ee8a48c5bb5d1
victortaira <53912934+victortaira@users.noreply.github.com>
2019-08-08 15:53:46 -0300

    Update jenkins-pipeline.groovy

--- jenkins-pipeline.groovy
+++ jenkins-pipeline.groovy
@@ -1,7 +1,7 @@
  node('maven') {
     stage('build') {
         echo 'building app :)'
-        openshiftBuild(buildConfig: 'workshop-ocp', showBuildLogs: 'true')
+        openshiftBuild(buildConfig: 'app-s2i', showBuildLogs: 'true')
     }
     stage('verify') {
         echo 'dummy verification....'
